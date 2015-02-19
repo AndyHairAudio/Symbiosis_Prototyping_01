f74289520eae0b197db8993879d6189233ff7dd2
Tamerlane92 <bluntsandpunks@gmail.com>
2015-02-19 15:31:01 +0000

    Implementing Forest Ambient Insects

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/English(US)/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/English(US)/Wwise_IDs.h
@@ -13,31 +13,13 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID PLAY_AMB_CAMPFIRE_CRACKLE = 1937775604U;
-        static const AkUniqueID PLAY_AMB_FOREST_DAY_BACKGROUND_LOOP = 3359688453U;
-        static const AkUniqueID PLAY_AMB_TREE_RUSTLE = 3748981955U;
-        static const AkUniqueID PLAY_STEMPICKER_BIGTREE = 2623417700U;
-        static const AkUniqueID PLAY_STEMPICKER_BUSH = 1405335252U;
-        static const AkUniqueID PLAY_STEMPICKER_MEDIUMTREE = 2496235435U;
-        static const AkUniqueID PLAY_STEMPICKER_SAPLING = 3256481050U;
-        static const AkUniqueID PLAY_STEMPICKER_SMALLTREE = 1574472763U;
-        static const AkUniqueID STOP_STEMPICKER_BIGTREE = 3741724626U;
-        static const AkUniqueID STOP_STEMPICKER_BUSH = 4078466226U;
-        static const AkUniqueID STOP_STEMPICKER_MEDIUMTREE = 3142720133U;
-        static const AkUniqueID STOP_STEMPICKER_SAPLING = 3944943176U;
-        static const AkUniqueID STOP_STEMPICKER_SMALLTREE = 3195754805U;
+        static const AkUniqueID PLAY_CHIRP = 3187155090U;
     } // namespace EVENTS
-
-    namespace GAME_PARAMETERS
-    {
-        static const AkUniqueID NEW_GAME_PARAMETER = 3671138082U;
-    } // namespace GAME_PARAMETERS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID AMB_BACKGROUND = 1184774098U;
-        static const AkUniqueID AMB_EMITTERS = 316118591U;
+        static const AkUniqueID FOREST_FLOOR_AMBIENCE = 3578186316U;
     } // namespace BANKS
 
     namespace BUSSES
