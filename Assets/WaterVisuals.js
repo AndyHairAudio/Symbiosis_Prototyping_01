da2f70658ea49c500b9337cbadf626ad4d031e78
Tamerlane92 <bluntsandpunks@gmail.com>
2015-04-01 00:56:57 +0100

    mo things

---
+++ Assets/WaterVisuals.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var colorComp : ColorCorrectionCurves;
+	colorComp = gameObject.GetComponent(ColorCorrectionCurves);
+	var blurComp : Blur;
+	blurComp = gameObject.GetComponent(Blur);
+function Start () {
+	
+}
+
+function Update () {
+
+}
+
+function OnTriggerStay (other : Collider){
+	if (other.tag == "LakeWaterCollider") {
+			colorComp.enabled = true;
+			blurComp.enabled = true;
+		}
+}
+
+function OnTriggerExit (other : Collider){
+	if (other.tag == "LakeWaterCollider") {
+			colorComp.enabled = false;
+			blurComp.enabled = false;
+		}
+
+}
